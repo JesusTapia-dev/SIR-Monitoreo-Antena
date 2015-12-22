59d71b178000d5ed47b127ece6320f9dac94ea79
Fiorella Quino <fiorella.quino@jro.igp.gob.pe>
2015-12-22 15:52:23 +0000

    Se quito Jsonfield
    git-svn-id: http://jro-dev.igp.gob.pe/svn/jro_hard/radarsys/trunk/webapp@20 aa17d016-51d5-4e8b-934c-7b2bbb1bbe71

--- apps/cgs/models.py
+++ apps/cgs/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from apps.main.models import Configuration
-from json_field import JSONField
+#from json_field import JSONField
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
@@ -16,7 +16,7 @@ class CGSConfiguration(Configuration):
     freq1 = models.FloatField(verbose_name='Frequency 1',validators=[MinValueValidator(62.5e6), MaxValueValidator(450e6)])
     freq2 = models.FloatField(verbose_name='Frequency 2',validators=[MinValueValidator(62.5e6), MaxValueValidator(450e6)])
     freq3 = models.PositiveIntegerField(verbose_name='Frequency 3',validators=[MinValueValidator(62.5e6), MaxValueValidator(450e6)])
-    freqs = JSONField(default={"frequencies":[{"f0":freq0,"f1":freq1,"f2":freq2,"f3":freq3}]}, blank=True)
+    #jfreqs = JSONField(default={"frequencies":[{"f0":freq0,"f1":freq1,"f2":freq2,"f3":freq3}]}, blank=True)
     #clk_in = models.PositiveIntegerField(default=10e6)
     #mult = models.PositiveIntegerField(default=40)
     #div = models.PositiveIntegerField(default=1)
