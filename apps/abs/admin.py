226609c483af0d9a9e75b15338e3eb1539d1a7d2
Fiorella Quino <fiorella.quino@jro.igp.gob.pe>
2016-08-31 19:57:47 +0000

    ABSConfiguration and ABSBeam have been added
    git-svn-id: http://jro-dev.igp.gob.pe/svn/jro_hard/radarsys/trunk/webapp@187 aa17d016-51d5-4e8b-934c-7b2bbb1bbe71

--- apps/abs/admin.py
+++ apps/abs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import ABSConfiguration
+from .models import ABSConfiguration, ABSBeam
 
 # Register your models here.
 
 admin.site.register(ABSConfiguration)
+admin.site.register(ABSBeam)
