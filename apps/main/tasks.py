97011dacc8aba4a1b7fc9ab221103eb010d53308
Fiorella Quino <fiorella.quino@jro.igp.gob.pe>
2017-03-10 20:33:41 +0000

    task for monitoring devices status from experiment
    git-svn-id: http://jro-dev.igp.gob.pe/svn/jro_hard/radarsys/trunk/webapp@279 aa17d016-51d5-4e8b-934c-7b2bbb1bbe71

--- apps/main/tasks.py
+++ apps/main/tasks.py
@@ -1,21 +1,38 @@
 from __future__ import absolute_import
 
 from celery import task
+from datetime import timedelta, datetime
 
 from .models import Experiment
 
 @task
 def task_start(id_exp):
-            
+
     exp = Experiment.objects.get(pk=id_exp)
-    
+
     return exp.start()
-    
+
 @task
 def task_stop(id_exp):
-            
-    exp = Experiment.objects.get(pk=id_exp)
-    
-    return exp.stop()
-    
 
+    exp = Experiment.objects.get(pk=id_exp)
+
+    return exp.stop()
+
+
+#Task to get status
+@task
+def task_status(id_exp):
+
+    exp = Experiment.objects.get(pk=id_exp)
+    if exp.status==2:
+        run_every = timedelta(minutes=1)
+        now = datetime.utcnow()
+        date = now + run_every
+        task_status.apply_async((id_exp,), eta=date)
+        print "Monitoring..."
+        exp.get_status()
+        return exp.status
+
+    else:
+        return exp.status
