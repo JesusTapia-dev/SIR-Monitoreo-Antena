4a39ad62697a1772c943f89ea638569a0d96b1af
Fiorella Quino <fiorella.quino@jro.igp.gob.pe>
2017-09-07 21:03:18 +0000

    Task #1119: bugs
    git-svn-id: http://jro-dev.igp.gob.pe/svn/jro_hard/radarsys/trunk/webapp@383 aa17d016-51d5-4e8b-934c-7b2bbb1bbe71

--- apps/jars/urls.py
+++ apps/jars/urls.py
@@ -4,12 +4,9 @@ from apps.jars import views
 
 urlpatterns = (
     url(r'^(?P<id_conf>-?\d+)/$', views.jars_conf, name='url_jars_conf'),
-    url(r'^(?P<id_conf>-?\d+)/edit/$', views.jars_conf_edit, name='url_edit_jars_conf'),
-    url(r'^(?P<conf_id>-?\d+)/new_filter/$', views.new_filter, name='url_new_jars_filter'),
+    url(r'^(?P<id_conf>-?\d+)/edit/$', views.jars_conf_edit, name='url_edit_jars_conf'),    
     url(r'^(?P<conf_id>-?\d+)/change_filter/$', views.change_filter, name='url_change_jars_filter'),
     url(r'^(?P<conf_id>-?\d+)/change_filter/(?P<filter_id>-?\d+)/$', views.change_filter, name='url_change_jars_filter'),
-    url(r'^(?P<conf_id>-?\d+)/view_filter/(?P<filter_id>-?\d+)/$', views.view_filter, name='url_jars_filter'),
-    url(r'^(?P<conf_id>-?\d+)/view_filter/(?P<filter_id>-?\d+)/edit$', views.edit_filter, name='url_edit_jars_filter'),
     url(r'^(?P<conf_id>-?\d+)/import/$', views.import_file, name='url_import_jars_conf'),
     url(r'^(?P<conf_id>-?\d+)/read/$', views.read_conf, name='url_read_jars_conf'),
 )
--- apps/jars/views.py
+++ apps/jars/views.py
@@ -165,109 +165,6 @@ def read_conf(request, conf_id):
 
     return render(request, 'jars_conf_edit.html', kwargs)
 
-def view_filter(request, conf_id, filter_id):
-
-    conf = get_object_or_404(JARSConfiguration, pk=conf_id)
-    filter = get_object_or_404(JARSfilter, pk=filter_id)
-
-    filter_parms       = eval(conf.filter_parms)
-    if filter_parms.__class__.__name__=='str':
-        filter_parms       = eval(filter_parms)
-    #filter.name        = filter_parms['name']
-    filter.clock       = filter_parms['clock']
-    filter.mult        = filter_parms['mult']
-    filter.fch         = filter_parms['fch']
-    filter.fch_decimal = filter_parms['fch_decimal']
-    filter.filter_fir  = filter_parms['filter_fir']
-    filter.filter_2    = filter_parms['filter_2']
-    filter.filter_5    = filter_parms['filter_5']
-
-    kwargs = {}
-    kwargs['conf']          = conf
-    kwargs['filter']        = filter
-    kwargs['dev_conf']      = filter
-    kwargs['dev_conf_keys'] = ['clock', 'mult', #'name',
-                               'fch', 'fch_decimal',
-                               'filter_2', 'filter_5',
-                               'filter_fir']
-
-    kwargs['title']         = 'Filter View'
-    kwargs['suptitle']      = 'Details'
-    kwargs['button']        = 'SI'
-    kwargs['edit_button']   = 'Edit Filter'
-    kwargs['add_button']    = 'New Filter'
-
-    return render(request, 'jars_filter.html', kwargs)
-
-def edit_filter(request, conf_id, filter_id):
-
-    conf = get_object_or_404(JARSConfiguration, pk=conf_id)
-    filter_parms = eval(conf.filter_parms)
-
-    if filter_id:
-        filter = get_object_or_404(JARSfilter, pk=filter_id)
-
-    if request.method=='GET':
-        form = JARSfilterForm(initial=filter_parms)
-
-    if request.method=='POST':
-        parms = {}
-        #parms['name']        = request.POST['name']
-        parms['clock']       = request.POST['clock']
-        parms['mult']        = request.POST['mult']
-        parms['fch']         = request.POST['fch']
-        parms['fch_decimal'] = request.POST['fch_decimal']
-        parms['filter_fir'] = request.POST['filter_fir']
-        parms['filter_2']   = request.POST['filter_2']
-        parms['filter_5']   = request.POST['filter_5']
-
-        conf.filter_parms = parms
-        conf.save()
-
-        #form = JARSfilterForm(request.POST)
-        #form = JARSfilterForm(request.POST, instance=filter)
-        #if form.is_valid():
-            #form.save()
-        #    messages.success(request, 'JARS Filter successfully updated')
-        #    return redirect('url_jars_filter', conf.id, filter.id)
-        return redirect('url_jars_filter', conf.id, filter.id)
-
-    kwargs = {}
-    kwargs['form'] = form
-    kwargs['title'] = conf.name
-    kwargs['suptitle'] = 'Edit Filter'
-    kwargs['button'] = 'Save'
-   # kwargs['previous'] = conf.get_absolute_url_edit()
-    kwargs['dev_conf'] = conf
-
-    return render(request, 'jars_filter_edit.html', kwargs)
-
-def new_filter(request, conf_id):
-
-    conf = get_object_or_404(JARSConfiguration, pk=conf_id)
-
-    if request.method=='GET':
-        form = JARSfilterForm()
-
-    if request.method=='POST':
-        form = JARSfilterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            new_filter  = get_object_or_404(JARSfilter, name=request.POST['name'])
-            conf.filter = new_filter
-            conf.add_parms_to_filter()
-            messages.success(request, 'New JARS Filter successfully created')
-            return redirect('url_edit_jars_conf', id_conf=conf.id)
-
-    kwargs = {}
-    kwargs['form'] = form
-    kwargs['title'] = 'New Filter'
-    kwargs['suptitle'] = ''
-    kwargs['button'] = 'Create'
-   # kwargs['previous'] = conf.get_absolute_url_edit()
-    kwargs['dev_conf'] = conf
-
-    return render(request, 'jars_new_filter.html', kwargs)
 
 
 def change_filter(request, conf_id, filter_id=None):
@@ -298,8 +195,9 @@ def change_filter(request, conf_id, filter_id=None):
             conf.filter_parms = json.dumps(jars_filter)
             conf.save()
             return redirect('url_edit_jars_conf', id_conf=conf.id)
-
-        print jars_filter
+        else:
+            messages.error(request, "Select a Filter Template")
+            return redirect('url_change_jars_filter', conf_id=conf.id)
 
     kwargs = {}
     kwargs['title'] = 'JARS Configuration'
