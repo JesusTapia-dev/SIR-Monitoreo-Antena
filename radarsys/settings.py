a377df340a376be94d596976ad331e7f21ec007a
Renato-TUF <nerofourm@gmail.com>
2023-01-18 11:39:06 -0500

    Pareciera tener menos errores para levantar la web

--- radarsys/settings.py
+++ radarsys/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap4',
+    'django-bootstrap5',
     'polymorphic',
     'apps.accounts',
     'apps.main',
