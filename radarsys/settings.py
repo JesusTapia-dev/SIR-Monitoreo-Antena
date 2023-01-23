78b76c52da1f4ed05ec29e0a265afa45980f9a07
Renato-TUF <nerofourm@gmail.com>
2023-01-23 13:48:08 -0500

    Se cambió de medium a md en bootstrap para todos los templates, falla al momento de buscar en base a filtros (boton de la lupa) por modificar el diccionario mientras se lee

--- radarsys/settings.py
+++ radarsys/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_bootstrap5',
-    'graphene_django',
     'polymorphic',
     'apps.accounts',
     'apps.main',
