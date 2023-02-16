f57fc19d5552df77c653bbb34542e947f00af937
Renato Huallpa <nerofourm@gmail.com>
2023-02-16 08:52:12 -0500

    Se corrigió atrad al añadirle carpeta de migraciones

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ python3 manage.py collectstatic --no-input
 #DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
 
 gunicorn -k eventlet radarsys.wsgi:application --bind 0.0.0.0:8000
- 
+    
